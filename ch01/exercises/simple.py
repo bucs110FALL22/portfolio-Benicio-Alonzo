f7a10103ca843bc0f75c71b3185106209f9408fd
Benicio Alonzo <112440198+Benicio-Alonzo@users.noreply.github.com>
2022-09-06 21:38:35 +0000

    Completed working with data

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -1,1 +1,22 @@
-print(7//4)
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15) # it is a repeating decimal
+
+
+rate = float(input( "What is the current exchange rate for the Euro to Dollar? "))
+
+
+amount = float(input("What is the amount of currency that needs to be exchanged? "))
+
+total= rate*amount
+
+result = total-3 # the 3 is for a service fee
+
+print("Your amount that has been exchanged is $",result)
--- main.py
+++ main.py
@@ -4,3 +4,5 @@ print("Hello World")
 # a
 # b
 # c
+
+print('1'+'1')
