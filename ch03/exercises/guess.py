7f8e49b57b2c670b0921f59e727ec21d6ef05692
Benicio Alonzo <112440198+Benicio-Alonzo@users.noreply.github.com>
2022-09-19 21:55:44 +0000

    finished chapter 3 exercise

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,5 +1,26 @@
 import random 
 
-guess = int(input("Guess what number the is "))
+num = random.randrange(1,11)
+num_guesses = 0
+correct_guess = False
 
-If 
+
+
+for i in range(3):
+  if not correct_guess:
+    guess_num = int(input("Please enter a guess from 1-10:"))
+   #num_guesses += 1
+  if guess_num > num:
+      print("your guess is too high")
+  elif guess_num < num:
+    print("your guess is too low")
+  else:
+    print("YOU ARE CORRECT")
+    break
+  
+
+
+
+
+
+#If the user guesses on the first try you can use a break to stop the loop
